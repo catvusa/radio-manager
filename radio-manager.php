a2bc1ebb73577ad16190d73d8f8b3f05f86db44f
Karel Vrabec <karelvrabeckv@gmail.com>
2020-10-18 17:16:44 +0200

    Basics

---
+++ radio-manager.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:       Radio Manager
+ * Plugin URI:        https://catvusa.com/
+ * Description:       A plugin for managing your own custom web radios.
+ * Version:           0.0.1
+ * Requires at least: 5.5
+ * Requires PHP:      7.3
+ * Author:            Karel Vrabec
+ * Author URI:        https://github.com/karelvrabeckv
+ */
+
+# Prevent a public user from direct access
+defined( 'ABSPATH' ) or die( 'Forbidden' );
